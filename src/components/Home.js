8d3603b48746be6ce7719d67b5d080c95b38b9f6
Johan Altamar <johan.altamar.dev@gmail.com>
2020-06-06 20:47:11 -0500

    Hero component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from "./home/Hero"
 
 function Home() {
     return (
         <div>
-            This is the Home Window
+            <Hero />
         </div>
     )
 }
---
+++ src/components/home/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+import {Link} from "react-router-dom"
+import hero from "../../img/hero.png";
+
+const Hero = () => {
+  return (
+    <div id="hero">
+      <Image fluid src={hero} className="d-sm-none" />
+      <div id="hero-caption-container">
+        <span id="hero-caption">Vive la experiencia</span>
+      </div>
+      <div id="hero-button-container">
+        <Link to="/menu" id="hero-button">Ordena Aquí</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
