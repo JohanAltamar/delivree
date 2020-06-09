4c483234f892e285fffa220337af747b54467254
Johan Altamar <johan.altamar.dev@gmail.com>
2020-06-08 23:23:14 -0500

    Api created inside menu folder

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,7 +16,7 @@ const data = [
   },
   {
     id: 2,
-    category: "hotDogs",
+    category: "hot dogs",
     image:
       "https://firebasestorage.googleapis.com/v0/b/cloudstorage-abfc4.appspot.com/o/images%2Fmenu%2FhotDogs-mobile.png?alt=media&token=1bb01a7a-c13f-45dd-aa50-77fbc8572fbd",
   },
---
+++ src/components/menu/menuApi.js
@@ -0,0 +1,160 @@
+data = [
+    {
+        id: 0,
+        category: "burgers",
+        items:[
+            {
+                id: 0, 
+                name: 'Fully',
+                price: 25000
+            },
+            {
+                id: 1,
+                name: 'Domenica',
+                price: 22000
+            },
+            {
+                id: 2,
+                name: 'Frozen',
+                price: 20000
+            },
+            {
+                id: 3,
+                name: 'Crunchy',
+                price: 29000
+            }
+        ]
+    },
+    {
+        id: 1,
+        category: "cocktails",
+        items:[
+            {
+                id: 0, 
+                name: 'Tropical',
+                price: 25000
+            },
+            {
+                id: 1,
+                name: 'Domenica',
+                price: 22000
+            },
+            {
+                id: 2,
+                name: 'Frozen',
+                price: 20000
+            },
+            {
+                id: 3,
+                name: 'Crunchy',
+                price: 29000
+            }
+        ]
+    },
+    {
+        id: 2,
+        category: "hot dogs",
+        items:[
+            {
+                id: 0, 
+                name: 'Tropical Salad',
+                price: 25000
+            },
+            {
+                id: 1,
+                name: 'Domenica',
+                price: 22000
+            },
+            {
+                id: 2,
+                name: 'Frozen',
+                price: 20000
+            },
+            {
+                id: 3,
+                name: 'Crunchy',
+                price: 29000
+            }
+        ]
+    },
+    {
+        id: 3,
+        category: "pastas",
+        items:[
+            {
+                id: 0, 
+                name: 'Tropical Salad',
+                price: 25000
+            },
+            {
+                id: 1,
+                name: 'Domenica',
+                price: 22000
+            },
+            {
+                id: 2,
+                name: 'Frozen',
+                price: 20000
+            },
+            {
+                id: 3,
+                name: 'Crunchy',
+                price: 29000
+            }
+        ]
+    },
+    {
+        id: 4,
+        category: "salads",
+        items:[
+            {
+                id: 0, 
+                name: 'Tropical Salad',
+                price: 25000
+            },
+            {
+                id: 1,
+                name: 'Domenica',
+                price: 22000
+            },
+            {
+                id: 2,
+                name: 'Frozen',
+                price: 20000
+            },
+            {
+                id: 3,
+                name: 'Crunchy',
+                price: 29000
+            }
+        ]
+    },
+    {
+        id: 5,
+        category: "sandwich",
+        items:[
+            {
+                id: 0, 
+                name: 'Tropical Salad',
+                price: 25000
+            },
+            {
+                id: 1,
+                name: 'Domenica',
+                price: 22000
+            },
+            {
+                id: 2,
+                name: 'Frozen',
+                price: 20000
+            },
+            {
+                id: 3,
+                name: 'Crunchy',
+                price: 29000
+            }
+        ]
+    },
+]
+
+export default data;
