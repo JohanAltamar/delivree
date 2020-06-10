e928ef1cd1cf9c5e4eb2561bb5a24bcaec6878ff
Johan Altamar <johan.altamar.dev@gmail.com>
2020-06-09 19:18:48 -0500

    Modal for add products to cart

--- src/components/menu/Categories.js
+++ src/components/menu/Categories.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import Modal from "./ItemToCart";
+import Message from "./AddedToCart";
 import data from "./menuApi";
 
 function Categories() {
@@ -7,23 +9,46 @@ function Categories() {
   let category = data.filter((data) => data.category === categoryName);
   let items = category[0].items;
 
+  const [modalShow, setModalShow] = useState(false);
+  const [show, setShow] = useState(false);
+  const [selected, setSelected] = useState({});
+
+  const openModal = (item) => {
+    setModalShow(true);
+    setSelected(item);
+  };
+
+  const closeModal = () => {
+    setModalShow(false);
+  };
+
   return (
     <section
       id="menu-category-container"
       className="brand-font-family brand-color-secondary"
     >
+      <Message show={show} delay={3000} onClose={() => setShow(false)} />
       <h3 className="font-weight-bold text-capitalize text-center">
         {categoryName}
       </h3>
       <section id="menu-category-items-container">
-      {items.map((product) => (
-        <figure key={product.id}>
-          <img src={product.url} alt={product.name} width="100%" />
-          <figcaption><strong>{product.name}</strong></figcaption>
-          <figcaption>Price: {product.price}</figcaption>
-        </figure>
-      ))}
+        {items.map((product) => (
+          <figure key={product.id} onClick={() => openModal(product)}>
+            <img src={product.url} alt={product.name} width="100%" />
+            <figcaption>
+              <strong>{product.name}</strong>
+            </figcaption>
+            <figcaption>Price: {product.price}</figcaption>
+          </figure>
+        ))}
       </section>
+      <Modal
+        id="item-modal"
+        show={modalShow}
+        onHide={() => closeModal()}
+        onAdd={() => setShow(true)}
+        item={selected}
+      />
     </section>
   );
 }
