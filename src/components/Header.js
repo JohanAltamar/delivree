1a14b629b8642bbf8b01265222284ff8f0ed68a1
Johan Altamar <johan.altamar.dev@gmail.com>
2020-06-11 19:19:28 -0500

    better nav header semantic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,47 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import BurgerIcon from "./header/BurgerMenu";
 import { connect } from "react-redux";
 import { toggleMenu } from "../redux/actions";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 function Header({ toggleMenu }) {
   return (
-    <Container fluid>
+    <header>
       <Row id="header">
         <Col xs={12} sm={{ offset: 1, span: 4 }}>
-          <header>
-            <h1 className="brand-name brand-color-main d-flex justify-content-center justify-content-sm-start align-items-center font-weight-bold text-decoration-none">
-              <Link to='/'>Foodies</Link>
-            </h1>
-            <div
-              id="burger-icon"
-              className="d-sm-none"
-              onClick={() => toggleMenu()}
-            >
-              <BurgerIcon />
-            </div>
-          </header>
+          <h1 className="brand-name brand-color-main d-flex justify-content-center justify-content-sm-start align-items-center font-weight-bold text-decoration-none">
+            <Link to="/">Foodies</Link>
+          </h1>
+          <div
+            id="burger-icon"
+            className="d-sm-none"
+            onClick={() => toggleMenu()}
+          >
+            <BurgerIcon />
+          </div>
         </Col>
         <Col
           sm={{ offset: 1, span: 6 }}
-          className="d-none d-sm-flex justify-content-around align-items-center brand-color-main text-decoration-none" 
+          className="d-none d-sm-flex justify-content-around align-items-center brand-color-main text-decoration-none"
         >
-          <h6><Link to="/">Inicio</Link></h6>
-          <h6><Link to="/menu">Menu</Link></h6>
-          <h6><Link to="/order">Carrito</Link></h6>
-          <h6><Link to="/login">Ingreso</Link></h6>
+          <nav>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/order">Carrito</Link>
+            </li>
+            <li>
+              <Link to="/login">Ingreso</Link>
+            </li>
+          </nav>
         </Col>
       </Row>
-    </Container>
+    </header>
   );
 }
 const mapDispatchToProps = (dispatch) => {
