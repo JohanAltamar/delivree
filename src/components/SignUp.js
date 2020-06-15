2b521535155e44f14b2e4af366c3ffaed5d03288
Johan Altamar <johan.altamar.dev@gmail.com>
2020-06-15 12:22:36 -0500

    Create mew users to firebase db and redirection to login page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,33 +1,55 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { auth } from "../services/firebase";
+import { Link, Redirect } from "react-router-dom";
+import db, { auth } from "../services/firebase";
+import { newUser, loggedUser } from "../redux/actions";
+
+// auth.onAuthStateChanged removed due to signUp component will be rendered
+// just when nobody is logged in. If some user is logged in, login page must
+// redirect the user to the profile page.
 
 export const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const initialState = {
+    fullname: "",
+    address: "",
+    telephone: "",
+    email: "",
+    password: "",
+    city: "",
+    neighborhood: "",
+  };
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
 
-  const onEmailChange = (event) => setEmail(event.target.value);
-  const onPasswordChange = (event) => setPassword(event.target.value);
+  const user = useSelector((state) => state.newUser);
+  const dispatch = useDispatch();
 
   const onSignUp = () => {
-    // console.log('Sign Up');
-    // console.log(email, password);
-
-    auth
-      .createUserWithEmailAndPassword(email, password)
+    auth.createUserWithEmailAndPassword(user.email, user.password)
+      .then(function (createdUser) {
+        let userRef = db.collection("users").doc(auth.currentUser.uid)
+          .collection("information");
+        let payload = user;
+        userRef
+          .add(payload)
+          .then(function (doc) {
+            // console.log("Document written with ID: ", doc.id);
+            setUserLoggedIn(true); //New User info added to database successfully
+            dispatch(loggedUser(user));
+            dispatch(newUser('all',initialState));
+          })
+          .catch(function (error) {
+            console.error("Error adding document: ", error);
+          });
+      })
       .catch(function (error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log("Error with Sign Up", errorCode, errorMessage);
-        // ...
       });
-    setEmail("");
-    setPassword("");
   };
+
   return (
     <div id="login-container">
       <Helmet>
@@ -40,31 +62,82 @@ export const SignUp = () => {
       <Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Correo Electrónico</Form.Label>
-          <Form.Control type="email" placeholder="name@foodies.com" value={email} onChange={onEmailChange}/>
+          <Form.Control
+            type="email"
+            placeholder="name@foodies.com"
+            value={user.email}
+            onChange={(event) => dispatch(newUser("email", event.target.value))}
+            required
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" placeholder="Password" value={password} onChange={onPasswordChange}/>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={(event) =>
+              dispatch(newUser("password", event.target.value))
+            }
+            required
+          />
         </Form.Group>
         <Form.Group controlId="user-name">
           <Form.Label>Nombre Completo</Form.Label>
-          <Form.Control type="input" placeholder="John Doe" />
+          <Form.Control
+            type="input"
+            placeholder="John Doe"
+            value={user.fullname}
+            onChange={(event) =>
+              dispatch(newUser("fullname", event.target.value))
+            }
+            required
+          />
         </Form.Group>
         <Form.Group controlId="user-phone">
           <Form.Label>Telefono</Form.Label>
-          <Form.Control type="input" placeholder="3055622663" />
+          <Form.Control
+            type="input"
+            placeholder="3055622663"
+            value={user.telephone}
+            onChange={(event) =>
+              dispatch(newUser("telephone", event.target.value))
+            }
+            required
+          />
         </Form.Group>
         <Form.Group controlId="user-address">
           <Form.Label>Dirección</Form.Label>
-          <Form.Control type="input" placeholder="Calle 50 # 25 - 56" />
+          <Form.Control
+            type="input"
+            placeholder="Calle 50 # 25 - 56"
+            value={user.address}
+            onChange={(event) =>
+              dispatch(newUser("address", event.target.value))
+            }
+            required
+          />
         </Form.Group>
         <Form.Group controlId="user-town">
           <Form.Label>Barrio</Form.Label>
-          <Form.Control type="input" placeholder="Villa Hermosa" />
+          <Form.Control
+            type="input"
+            placeholder="Villa Hermosa"
+            value={user.neighborhood}
+            onChange={(event) =>
+              dispatch(newUser("neighborhood", event.target.value))
+            }
+            required
+          />
         </Form.Group>
         <Form.Group controlId="user-city">
           <Form.Label>Municipio</Form.Label>
-          <Form.Control type="input" placeholder="Barranquilla" />
+          <Form.Control
+            type="input"
+            placeholder="Barranquilla"
+            value={user.city}
+            onChange={(event) => dispatch(newUser("city", event.target.value))}
+          />
         </Form.Group>
 
         <Form.Group>
@@ -76,12 +149,9 @@ export const SignUp = () => {
           Crear
         </Button>
       </Form>
+      {userLoggedIn && (<Redirect to="/login"/>)}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
+export default SignUp;
