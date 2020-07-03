179c8067fbfbfc5950f11010f44e473665a38349
Johan Altamar <johan.altamar.dev@gmail.com>
2020-07-03 02:43:47 -0500

    test add 2 home screen PWA feature test

--- src/App.js
+++ src/App.js
@@ -23,17 +23,21 @@ function App({ toggleMenu }) {
 
   function addToHomeScreen() {
    var a2hsBtn = document.querySelector(".ad2hs-prompt");
-   // hide our user interface that shows our A2HS button a2hsBtn.style.display = 'none';
-   // Show the prompt deferredPrompt.prompt();
-   // Wait for the user to respond to the prompt deferredPrompt.userChoice .then(function(choiceResult){
-   if (choiceResult.outcome === 'accepted') {
-     console.log('User accepted the A2HS prompt');
-   }
-   else {
-     console.log('User dismissed the A2HS prompt');
-   }
-   deferredPrompt = null;
+   // hide our user interface that shows our A2HS button
+   a2hsBtn.style.display = 'none';
+   // Show the prompt
+   deferredPrompt.prompt();
+   // Wait for the user to respond to the prompt
+   deferredPrompt.userChoice .then(function(choiceResult){
+     if (choiceResult.outcome === 'accepted') {
+       console.log('User accepted the A2HS prompt');
+     }
+     else {
+       console.log('User dismissed the A2HS prompt');
+     }
+     deferredPrompt = null;
   };
+}
 
   return (
     <main>
