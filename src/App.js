72c2dabcb642f7ff787fbe6e877d25b8ad84bfff
Johan Altamar <johan.altamar.dev@gmail.com>
2020-07-03 02:00:31 -0500

    test add 2 home screen PWA feature

--- src/App.js
+++ src/App.js
@@ -6,32 +6,40 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App({ toggleMenu }) {
-  window.onload = (e) => {
-    let deferredPrompt;
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-    });
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
+  var deferredPrompt;
+  window.addEventListener('beforeinstallprompt', function (e) {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    showAddToHomeScreen();
+  });
+
+  function showAddToHomeScreen() {
+    var a2hsBtn = document.querySelector(".ad2hs-prompt");
+    a2hsBtn.style.display = "block";
+    a2hsBtn.addEventListener("click", addToHomeScreen);
   }
+
+  function addToHomeScreen() {
+   var a2hsBtn = document.querySelector(".ad2hs-prompt");
+   // hide our user interface that shows our A2HS button a2hsBtn.style.display = 'none';
+   // Show the prompt deferredPrompt.prompt();
+   // Wait for the user to respond to the prompt deferredPrompt.userChoice .then(function(choiceResult){
+   // if (choiceResult.outcome === 'accepted') {
+   //   console.log('User accepted the A2HS prompt');
+   // }
+   // else {
+   //   console.log('User dismissed the A2HS prompt');
+   // }
+   deferredPrompt = null;
+  };
+
   return (
     <main>
-      {process.env.REACT_APP_HOLA}
       <Header />
       <Main />
+      <button className="ad2hs-prompt" style={{position:"absolute", display:"none", top:"50%", left:"50%"}}>Instalar</button>
     </main>
   );
 }
