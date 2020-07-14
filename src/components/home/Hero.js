0dadae3fac8c29eebb3668136bc0b5eec3ffbafd
Johan Altamar <johan.altamar.dev@gmail.com>
2020-07-14 12:45:03 -0500

    hero image minor mods

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Image } from "react-bootstrap";
 import {Link} from "react-router-dom"
 import hero from "../../img/restaurantHero2.webp";
+import "./hero.css"
 
 const Hero = () => {
   return (
     <div id="hero">
-      <Image fluid src={hero} className="d-sm-none" alt="hamburguesa foodies"/>
+      <Image id="hero-image" src={hero} className="d-sm-none" alt="hamburguesa foodies"/>
       <div id="hero-caption-container">
         <span id="hero-caption">Vive la experiencia</span>
       </div>
