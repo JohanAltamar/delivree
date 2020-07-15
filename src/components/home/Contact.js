9d1013a2690fd19b514f42477209b57222cdf87b
Johan Altamar <johan.altamar.dev@gmail.com>
2020-07-15 16:24:49 -0500

    location, working hours and contact mods

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Contact() {
     return (
         <section id="contact" className="brand-color-main brand-font-family">
-            <h2 className="font-weight-bold">Nuestro Contacto</h2>
-            <h6>Tel: 3562565</h6>
-            <h6>Cel: 301-257-8990</h6>
+            <h3 className="font-weight-bold">Nuestro Contacto</h3>
+            <h6><strong>Tel:</strong> 3562565</h6>
+            <h6><strong>Cel:</strong> 301-257-8990</h6>
         </section>
     )
 }
--- src/components/home/WorkingHours.js
+++ src/components/home/WorkingHours.js
@@ -3,7 +3,7 @@ import React from 'react'
 function WorkingHours() {
     return (
         <section id="working-hours" className="brand-font-family brand-color-main ">
-            <h2 className="font-weight-bold">Nuestros Horarios</h2>
+            <h3 className="font-weight-bold">Nuestros Horarios</h3>
             <ul>
                 <li><strong>Lun - Jue:</strong> 12:00PM - 11:00PM</li>
                 <li><strong>Vie - Dom:</strong> 11:00AM - 02:00AM</li>
