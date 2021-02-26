82679ecbf91422151c74ebe9fc1ce9434a6269a7
Johan Altamar <johan.altamar.dev@gmail.com>
2021-02-26 10:10:19 -0500

    react toastify added and set

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 import AppRouter from "./routers/AppRouter";
 
@@ -6,6 +8,7 @@ function App() {
   return (
     <>
       <AppRouter />
+      <ToastContainer autoClose={3000} />
     </>
   );
 }
--- src/redux2/sagas/cartSaga.js
+++ src/redux2/sagas/cartSaga.js
@@ -1,9 +1,11 @@
+import { toast } from "react-toastify";
 import { put, takeLatest } from "redux-saga/effects";
 import { clearSelectedProduct } from "../actions/productsActions";
 import types from "../types";
 
 function* addProduct2CartSaga() {
   yield put(clearSelectedProduct());
+  toast.success("Producto agregado al carrito.")
 }
 
 export default function* watcherCart() {
