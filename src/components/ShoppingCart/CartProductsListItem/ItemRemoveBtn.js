ce307ec14ade82f3ca2afd7f88e356df947bf7fe
Johan Altamar <johan.altamar.dev@gmail.com>
2021-02-27 12:13:02 -0500

    responsive shopping cart

--- src/components/ShoppingCart/CartProductsListItem/ItemRemoveBtn.js
+++ src/components/ShoppingCart/CartProductsListItem/ItemRemoveBtn.js
@@ -3,13 +3,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch } from "react-redux";
 import { removeCartProductAction } from "../../../redux2/actions/cartActions";
+import Swal from "sweetalert2";
 
 const ItemRemoveBtn = ({ id }) => {
   const dispatch = useDispatch();
 
   const handleRemoveClick = () => {
-    const confirmed = window.confirm("Desea eliminar el producto seleccionado");
-    if (confirmed) dispatch(removeCartProductAction(id));
+    Swal.fire({
+      title: "Estás seguro?",
+      text: "No podrás deshacerlo!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#00d000",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, elimínalo!",
+      cancelButtonText: "No, salgamos!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Borrado!", "El producto ha sido eliminado.", "success");
+        dispatch(removeCartProductAction(id));
+      }
+    });
   };
 
   return (
--- src/components/ShoppingCart/index.js
+++ src/components/ShoppingCart/index.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartProductsList from "./CartProductsList";
 
 const ShoppingCartContainer = () => {
-
   const { products } = useSelector((state) => state.shoppingCart);
 
+  const reducer = (accumulator, currentValue) =>
+    accumulator + currentValue.qty * currentValue.price;
+  const total = products.reduce(reducer, 0);
+
   return (
-    <section className="grid__padding">
+    <section className="cart__main-container grid__padding">
       {products.length === 0 ? (
         <h6>
           No hay productos en el carrito, agrégalos desde el{" "}
@@ -16,13 +19,19 @@ const ShoppingCartContainer = () => {
         </h6>
       ) : (
         <>
-          <h6 className="text-center">Productos Seleccionados</h6>
           <div className="cart__products-container">
+            <h6 className="text-center">Productos Seleccionados</h6>
             <CartProductsList products={products} />
+            <div className="cart__products-spacer"></div>
           </div>
-          <div className="cart__resume-container">
-            resumen
-            <button>Ordena Ahora</button>
+          <div className="cart__resume-wrapper">
+            <div className="cart__resume-container">
+              <h6 className="cart__resume-title">Resumen</h6>
+              <h5 className="cart__resume-total mb-3">
+                Total: $ <span>{total.toLocaleString("de-DE")}</span>
+              </h5>
+              <button className="btn btn-primary">Ordena Ahora</button>
+            </div>
           </div>
         </>
       )}
