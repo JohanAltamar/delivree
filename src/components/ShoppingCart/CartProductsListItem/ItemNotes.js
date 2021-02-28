5b68fca25f578483978d2c488cfa04ecfef407bc
Johan Altamar <johan.altamar.dev@gmail.com>
2021-02-28 06:10:36 -0500

    item notes autoclose if empty

--- src/components/ShoppingCart/CartProductsListItem/ItemNotes.js
+++ src/components/ShoppingCart/CartProductsListItem/ItemNotes.js
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faNotesMedical } from "@fortawesome/free-solid-svg-icons";
 
 const ItemNotes = ({ notes, notesChangeFnc }) => {
-  const [addNotes, setAddNotes] = useState(false);
+  const [addNotes, setAddNotes] = useState(notes ? true : false);
   const handleAddNoteClick = () => {
     setAddNotes(!addNotes);
   };
+
+  useEffect(() => {
+    if (notes.length === 0) setAddNotes(false);
+  }, [notes]);
 
   return (
     <>
       {(notes || addNotes) && (
         <span className="cart__products-item-notes mt-2">
-          <textarea value={notes} onChange={notesChangeFnc} />
+          <textarea
+            autoFocus
+            value={notes}
+            onChange={notesChangeFnc}
+            placeholder="Escriba su nota aquí"
+            maxLength={250}
+          />
         </span>
       )}
       {!notes && (
