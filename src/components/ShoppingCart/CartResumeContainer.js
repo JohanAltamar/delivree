fc2bec9e2442780361e1c365e85fe0f1ea10cbaa
Johan Altamar <johan.altamar.dev@gmail.com>
2021-02-28 06:11:23 -0500

    Link added to order now button

--- src/components/ShoppingCart/CartResumeContainer.js
+++ src/components/ShoppingCart/CartResumeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const CartResumeContainer = ({ total }) => {
   return (
@@ -9,7 +10,9 @@ const CartResumeContainer = ({ total }) => {
         <h5 className="cart__resume-total mb-3">
           Total: $ <span>{total.toLocaleString("de-DE")}</span>
         </h5>
-        <button className="btn btn-primary">Ordena Ahora</button>
+        <Link to="/cart/user-info">
+          <button className="btn btn-primary">Ordena Ahora</button>
+        </Link>
       </div>
     </div>
   );
