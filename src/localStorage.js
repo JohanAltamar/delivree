c37adec7482512d51baca1ff939901ce8b1ac1c6
Johan Altamar <johan.altamar.dev@gmail.com>
2021-03-01 20:22:56 -0500

    unused files removed

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,30 +1,29 @@
-import {initialState} from "./redux/reducer";
+// import {initialState} from "./redux/reducer";
+
+const initialState = {};
+
 export const loadState = () => {
-    try {
-        const serializedState = localStorage.getItem('state');
-        if(serializedState === null){
-            // console.log(initialState)
-            return initialState;
-        }
-        return JSON.parse(serializedState)
-    } catch (error) {
-        return initialState
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      // console.log(initialState)
+      return initialState;
     }
-}
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return initialState;
+  }
+};
 
 export const saveState = (state) => {
-    try {
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem('state', serializedState)
-    } catch (error) {
-
-    }
-}
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (error) {}
+};
 
 export const removeState = () => {
   try {
-      localStorage.removeItem('state')
-  } catch (e) {
-
-  }
-}
+    localStorage.removeItem("state");
+  } catch (e) {}
+};
