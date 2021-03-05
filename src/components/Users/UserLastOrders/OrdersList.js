95a69ceb32486eb35b1b3fd9e35182f0ed3f6045
Johan Altamar <johan.altamar.dev@gmail.com>
2021-03-05 05:22:51 -0500

    back button added

--- src/components/Users/UserLastOrders/OrdersList.js
+++ src/components/Users/UserLastOrders/OrdersList.js
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import OrdersListItem from "./OrdersListItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const OrdersList = ({ orders }) => {
   return orders?.length ? (
     <>
-      <h4 className="text-center">Pedidos recientes</h4>
+      <h4 className="text-center orders__last-orders-title">
+        <Link className="orders__last-orders-title-back-btn" to="/dashboard">
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </Link>
+        <span>Pedidos recientes</span>
+      </h4>
       {orders.map((order) => (
         <OrdersListItem orderInfo={order} key={order.orderID} />
       ))}
