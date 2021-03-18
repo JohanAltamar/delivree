a7f029148415767d199180a310bb19082c87512f
Johan Altamar <johan.altamar.dev@gmail.com>
2021-03-18 16:39:24 -0500

    latest orders updated

--- src/components/Users/UserLastOrders/index.js
+++ src/components/Users/UserLastOrders/index.js
@@ -1,22 +1,30 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { startFetchUserLatestOrdersAction } from "../../../redux2/actions/userActions";
 import OrdersList from "./OrdersList";
 
 const UserLastOrders = () => {
   const dispatch = useDispatch();
   const { id, latestOrders } = useSelector((state) => state.userInfo);
+  const { loading } = useSelector((state) => state.ui);
 
   useEffect(() => {
-    if (!latestOrders) {
+    const dispatchAction = !loading && latestOrders === undefined;
+    if (dispatchAction) {
       dispatch(startFetchUserLatestOrdersAction(id));
     }
-  }, [dispatch, id, latestOrders]);
+  }, [dispatch, id, latestOrders, loading]);
 
-  return (
+  return !loading && latestOrders && latestOrders.length > 0 ? (
     <>
       <OrdersList orders={latestOrders} />
     </>
+  ) : (
+    <h3 className="text-center margin-auto">
+      No hay ordenes recientes, empieza una desde nuestro
+      <Link to="/menu">Menú</Link>
+    </h3>
   );
 };
 
