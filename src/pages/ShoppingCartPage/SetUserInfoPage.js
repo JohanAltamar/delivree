037baba366a31305d307ca9ff64abdc7f9f7910b
Johan Altamar <johan.altamar.dev@gmail.com>
2021-03-30 16:54:53 -0500

    add login section

--- src/pages/ShoppingCartPage/SetUserInfoPage.js
+++ src/pages/ShoppingCartPage/SetUserInfoPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import LoginContainer from "../../components/Login";
 import SEO from "../../components/SEO";
 import GuestUserContainer from "../../components/ShoppingCart/GuestUserContainer";
 import useRedirectToCart from "../../hooks/useRedirectToCart";
@@ -12,7 +13,8 @@ const SetUserInfoPage = () => {
       <SEO title="Ingresa tus datos" />
       <Layout>
         <article className="cart__user-info-main-container grid__padding">
-          <div>Login Container</div>
+          {/* <div>Login Container</div> */}
+          <LoginContainer />
           <GuestUserContainer />
         </article>
       </Layout>
